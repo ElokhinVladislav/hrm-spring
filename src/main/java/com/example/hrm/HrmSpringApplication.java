072a0be1f6e4c0b23f76b223254f2a7035ad2481
ElokhinVladislav <@ElokhinVladislav>
2026-05-03 19:49:35 +0300

    init spring boot project

---
+++ src/main/java/com/example/hrm/HrmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmSpringApplication.class, args);
+    }
+
+}
